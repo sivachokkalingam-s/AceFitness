7dba27ce967c97b860f35c375bac6d76b8c1821c
sivachokkalingam-s <sivachokkalingam1510@gmail.com>
2026-04-30 02:12:49 +0530

    jenkins fix

--- Aceestver.py
+++ Aceestver.py
@@ -90,4 +90,4 @@ def save_progress():
 
 # ---------------- MAIN ----------------
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    app.run(host="127.0.0.1", port=5000)
